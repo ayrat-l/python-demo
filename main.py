4660534c6451aaae804b31512db525711aa84dcd
ayrat-l <kaz90@bk.ru>
2019-02-07 23:19:27 +0300

    Added # foк test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from lib import cashback
 # print(value)
 # print(cashback(10_000)) #alt + Enter
 # print(cashback(20_000))
-#
-# print('*******' * 3)
+
+print('*******' * 3)
 from lib import bmi
 print(bmi(75, 1.83))
