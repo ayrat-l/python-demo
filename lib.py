d3ea25139b6f421f921141ace6a51c1f2b981745
ayrat-l <kaz90@bk.ru>
2019-02-09 21:39:23 +0300

    Added new function

--- lib.py
+++ lib.py
@@ -1,3 +1,4 @@
+import math
 def cashback(monthly_expenses): #объявление + указание параметра
     """
     >>> cashback(10_000)
@@ -23,3 +24,10 @@ def distance_calculation(volume_fuel,expense_fuel):
     result = volume_fuel * 100 / expense_fuel
     return result
 
+def perimeter_of_the_room(width_of_the_room,length_of_the_room):
+    """
+    >>> perimeter_of_the_room(3,5)
+    16
+    """
+    perimeter_of_the_room = float(width_of_the_room + length_of_the_room) * 2
+    return perimeter_of_the_room
